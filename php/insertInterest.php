46619e3b758f84176410a5e6edf8e6e846c9b5a8
Timothy <esposit@audible.com>
2015-08-29 00:35:19 -0400

    This commit adds the functionality for submitting email for interested people in Apt.

--- php/insertInterest.php
+++ php/insertInterest.php
@@ -1,6 +1,22 @@
 <?php
-header('Content-Type: text/plain');
 
-echo hi;
+include ("account.php");
+include ("connect.php");
+
+if (empty($_POST['EMAIL'])) {
+	exit("emailEmpty");
+}
+
+if (empty($_POST['LOCATION'])) {
+	exit("locationEmpty");
+}
+
+$query = 'INSERT INTO INTERESTS VALUES ("", '.$_POST['EMAIL'].', "");';
+
+mysql_query($query) OR die("ERROR");
+
+print("SUCCESS");
+
+mysql_close();
 
 ?>
