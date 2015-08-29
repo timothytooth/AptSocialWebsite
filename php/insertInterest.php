4093ce4ab87d32dd281fa35635608d9d974418c5
Timothy <esposit@audible.com>
2015-08-29 00:39:27 -0400

    Blah.

--- php/insertInterest.php
+++ php/insertInterest.php
@@ -7,6 +7,8 @@ if (empty($_POST['EMAIL'])) {
 	exit("emailEmpty");
 }
 
+print($_POST['EMAIL']);
+
 $query = 'INSERT INTO INTERESTS VALUES ("", '.$_POST['EMAIL'].', "");';
 
 mysql_query($query) OR die("ERROR");
