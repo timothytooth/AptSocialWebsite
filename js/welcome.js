85c4d066040c9118757423674a9e79b26a6f03f1
Timothy <esposit@audible.com>
2015-09-12 21:18:21 -0400

    This adds needed Javascript functionality for fading in an informational dialog.

--- js/welcome.js
+++ js/welcome.js
@@ -2,8 +2,10 @@ function submitInterest() {
 	var emailField = $("#emailField").val();
 	var serializedData = { "EMAIL" : emailField };
 
-	$.post("php/insertInterest.php", serializedData, function(response) {
-    	// Log the response to the console
-    	console.log("Response: "+response);
-	}, "text");
+	if (emailField.trim()) {
+		$.post("php/insertInterest.php", serializedData, function(response) {
+    		$("#thanksContainer").html("<div id='thanksDialog' class='center'>Thanks! We'll be in touch.</div>");
+			$("#thanksDialog").fadeIn("slow").delay(4000).fadeOut("fadeOut");
+		}, "text");
+	}
 }
